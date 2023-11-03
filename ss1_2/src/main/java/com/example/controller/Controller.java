c8ac3c8af1a33429de27e53c8d25efd77e353de8
thuyyy555 <tthuynngo15101@gmail.com>
2023-11-03 20:55:55 +0700

    baitap

---
+++ ss1_2/src/main/java/com/example/controller/Controller.java
@@ -0,0 +1,48 @@
+package com.example.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
+@org.springframework.stereotype.Controller
+public class Controller {
+    String[] array1 = {"dog", "cat", "rabbit"};
+    String[] array2 = {"chó", "mèo", "thỏ"};
+    @PostMapping("/change")
+    public String findWord(@RequestParam String word, Model model) {
+        String result="";
+        if(find(word)){
+            result=getWord(word);
+        }
+        model.addAttribute("result", result);
+        return "index";
+    }
+    @GetMapping(value = "/change")
+    public String showForm(Model model){
+        return"index";
+    }
+    boolean find(String input){
+        boolean found = false;
+        for (int i = 0; i < array1.length; i++) {
+            if (input.equals(array1[i])) {
+                found = true;
+                break;
+            }
+        }
+        return found;
+    }
+    String getWord(String input){
+        String res="";
+        for (int i = 0; i < array1.length; i++) {
+            if (input.equals(array1[i])) {
+                int result = Arrays.asList(array1).indexOf(array1[i]);
+                res=array2[result];
+                break;
+            }
+        }
+        return res;
+    }
+}
---
+++ ss1_2/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,16 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JSP - Hello World</title>
+</head>
+<body>
+<h1>Từ điển</h1>
+<form action="/change" method="post">
+    Nhập từ: <input type="text" name="word" required>
+    Tra từ: <input type="submit">
+</form>
+Kết quả: <c:out value="${result}"/>
+</body>
+</html>
