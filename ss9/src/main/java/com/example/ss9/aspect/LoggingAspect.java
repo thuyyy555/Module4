71e9714d6ab3a21c55130ccf07528458f1c14909
thuyyy555 <tthuynngo15101@gmail.com>
2023-12-13 18:50:39 +0700

    baitap

--- ss9/src/main/java/com/example/ss9/aspect/LoggingAspect.java
+++ ss9/src/main/java/com/example/ss9/aspect/LoggingAspect.java
@@ -1,31 +1,52 @@
 package com.example.ss9.aspect;
 
+import com.example.ss9.model.Book;
+import com.example.ss9.service.BookService;
+import com.example.ss9.service.BookServiceImpl;
+import com.example.ss9.service.BorrowBService;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.sql.SQLOutput;
 
 @Aspect
 @Component
 public class LoggingAspect {
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    private BookService bookService;
+    @Autowired
+    private BorrowBService borrowBService;
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     private int visitorCount = 0;
 
-//    @Before("execution(* com.example.ss9.controller.LibraryController(..))")
-//    public void logBorrowBook(int bookId, int userId) {
-//        logger.info("Book with ID " + bookId + " is borrowed");
-//    }
-
-    @AfterReturning("execution(* com.example.ss9.controller.LibraryController.doBorrow(..))")
+    @Before("execution(* com.example.ss9.controller.LibraryController.doBorrow(..))")
     public void logVisitorCountAfterBorrow() {
         visitorCount++;
         logger.info("Visitor count: " + visitorCount);
     }
 
-//    @Before("execution(* com.example.ss9.controller.LibraryController.doReturn(..)) && args(bookId)")
-//    public void logReturnBook(int bookId) {
-//        logger.info("Book with ID " + bookId + " is returned");
-//    }
-
+    @AfterReturning(
+            pointcut = "execution(* com.example.ss9.controller.LibraryController.doBorrow(..)) && args(id, idBorrow)",
+            returning = "result",
+            argNames = "id,idBorrow,result")
+    public void afterBorrow(int id, int idBorrow, String result) {
+        Book book = bookService.findById(id);
+        System.out.println("Book co id la " + id + " da duoc muon");
+        System.out.println("So luong sach hien tai cua sach co id " + id + ": " + book.getQuantity());
+    }
+    @AfterReturning(
+            pointcut = "execution(* com.example.ss9.controller.LibraryController.doReturn(..)) && args(idBorrow)",
+            returning = "result",
+            argNames = "idBorrow,result")
+    public void afterReturn(int idBorrow, String result) {
+        int id = (borrowBService.findById(idBorrow)).getBook().getId();
+        Book book = bookService.findById(id);
+        System.out.println("Book co id la " + id + " da duoc tra");
+        System.out.println("So luong sach hien tai cua sach co id " + id + ": " + book.getQuantity());
+    }
 }
